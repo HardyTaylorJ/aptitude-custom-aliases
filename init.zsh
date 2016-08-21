7a30fb2311db3215a2fd6dfacb0c3069fb90d221
HardyTaylorJ <hardy.taylor.j@gmail.com>
2016-08-21 16:55:39 -0400

    initial add

---
+++ init.zsh
@@ -0,0 +1,19 @@
+
+alias yai='sudo aptitude install'
+alias yar='sudo aptitude remove'
+alias yap='sudo aptitude purge'
+alias yas='sudo aptitude search'
+#show details
+alias yad='sudo aptitude show'
+alias yay='sudo aptitude update'
+alias yau='sudo aptitude safe-upgrade'
+alias yauf='sudo aptitude full-upgrade'
+
+alias yah='sudo aptitude -h; echo "yai: install
+yar: remove
+yap: purge
+yas: search
+yad: show (show details of a package)
+yay: update
+yau: safe-upgrade
+yauf: full-upgrade (may remove packages)"'
