d70020cb363fe1f2f25e2f29df593c134f3e478a
HardyTaylorJ <hardy.taylor.j@gmail.com>
2016-08-24 16:05:47 -0400

    Added synchronize -> then [safe/full]upgrade options (yayu/yayuf) Added mneumonic aid to help command Added general info line to the help command

--- init.zsh
+++ init.zsh
@@ -7,13 +7,19 @@ alias yas='sudo aptitude search'
 alias yad='sudo aptitude show'
 alias yay='sudo aptitude update'
 alias yau='sudo aptitude safe-upgrade'
+alias yayu='sudo aptitude update; sudo aptitude safe-upgrade'
 alias yauf='sudo aptitude full-upgrade'
+alias yayuf='sudo aptitude update; sudo aptitude full-upgrade'
 
-alias yah='sudo aptitude -h; echo "yai: install
-yar: remove
-yap: purge
-yas: search
-yad: show (show details of a package)
-yay: update
-yau: safe-upgrade
-yauf: full-upgrade (may remove packages)"'
+alias yah='sudo aptitude -h; echo "
+Aptitude Aliases: corresponding letters in description capitalized for mneumonic help ;)
+yai: Install
+yar: Remove
+yap: Purge
+yas: Search
+yad: show (show Details of a package)
+yay: update (sYnchronize)
+yau: safe-Upgrade
+yayu: update (sYnchronize) -> safe-Upgrade
+yauf: Full-Upgrade (may remove packages)
+yayuf: update (sYnchronize) -> Full-Upgrade (may remove packages)"'
